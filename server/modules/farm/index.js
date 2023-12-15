0a53c0472de47b2626562a34cbd39e01fbe12373
Anh Khoa <dev.nakhoa@gmail.com>
2023-12-15 16:43:27 +0700

    Create model Farm

---
+++ server/modules/farm/index.js
@@ -0,0 +1,3 @@
+import { Farm } from "./model/farm.js";
+
+Farm();
--- server/modules/index.js
+++ server/modules/index.js
@@ -2,3 +2,4 @@ import "./user/index.js";
 import "./core/index.js";
 import "./org/index.js";
 import "./language/index.js";
+import "./farm/index.js";
